e067097cc42d7b2652cf71402d7a2dde40aa270c
bsa94 <banpreet_aulakh@sfu.ca>
2023-01-02 21:14:20 -0800

    Added AddItemActivity and listener. Changed app icons

--- app/src/main/java/com/project/genericchecklist/UI/CheckListActivity.java
+++ app/src/main/java/com/project/genericchecklist/UI/CheckListActivity.java
@@ -2,15 +2,43 @@ package com.project.genericchecklist.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.project.genericchecklist.R;
 
 public class CheckListActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab;
+    private ListView listView;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+        fab = findViewById(R.id.addItemButton);
+        listView = findViewById(R.id.itemListView);
+
+        addNewItemButton();
     }
+
+    private void addNewItemButton() {
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "Clicked Add item", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(context, AddItemActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
